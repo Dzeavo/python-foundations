767f649672d7b773fd49c687af193e08642bdcb7
Dzeavo <Dzeavo@gmail.com>
2026-01-21 21:43:15 +0300

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -1,1 +1,24 @@
+def basic_stats(numbers):
+    total = 0
+    count = 0
 
+    minimum = numbers[0]
+    maximum = numbers[0]
+
+    for n in numbers:
+        total += n
+        count += 1
+
+        if n < minimum:
+            minimum = n
+
+        if n > maximum:
+            maximum = n
+
+    mean = total / count
+
+    return mean, minimum, maximum
+
+
+data = [10, -5, 3, 7, 0]
+print(basic_stats(data))
