5c2faa7a3f7d4b54e852effde58da25b7b5bf628
Dzeavo <Dzeavo@gmail.com>
2026-01-21 21:50:23 +0300

    Update numpy_stats.py

--- numpy_stats.py
+++ numpy_stats.py
@@ -1,1 +1,15 @@
+import numpy as np
+
+def basic_stats_np(numbers):
+    data = np.array(numbers)
+
+    mean = data.mean()
+    minimum = data.min()
+    maximum = data.max()
+
+    return mean, minimum, maximum
+
+
+data = [10, -5, 3, 7, 0]
+print(basic_stats_np(data))
 
