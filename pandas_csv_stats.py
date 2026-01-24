c775cf5770fe0d565d2f288ada6366fd1d6f1a1a
Dzeavo <Dzeavo@gmail.com>
2026-01-24 15:24:31 +0300

    Update pandas_csv_stats.py

--- pandas_csv_stats.py
+++ pandas_csv_stats.py
@@ -1,1 +1,15 @@
+from typing import Tuple
+import pandas as pd
+
+def table_stats_from_csv(path: str) -> Tuple[float, float, float]:
+    df = pd.read_csv(path)
+
+    mean_salary = df["salary"].mean()
+    min_salary = df["salary"].min()
+    max_salary = df["salary"].max()
+
+    return mean_salary, min_salary, max_salary
+
+
+print(table_stats_from_csv("data.csv"))
 
