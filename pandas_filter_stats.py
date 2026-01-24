c4c12c60abc7c57e6ffc34fea5b692ac6ed6bde2
Dzeavo <Dzeavo@gmail.com>
2026-01-24 15:42:07 +0300

    Update pandas_filter_stats.py

--- pandas_filter_stats.py
+++ pandas_filter_stats.py
@@ -1,1 +1,18 @@
+from typing import Tuple
+import pandas as pd
+
+def table_stats_filtered(path: str) -> Tuple[float, float, float]:
+    df = pd.read_csv(path)
+
+    # фильтруем: только люди старше 30
+    filtered_df = df[df["age"] > 30]
+
+    mean_salary = filtered_df["salary"].mean()
+    min_salary = filtered_df["salary"].min()
+    max_salary = filtered_df["salary"].max()
+
+    return mean_salary, min_salary, max_salary
+
+
+print(table_stats_filtered("data.csv"))
 
