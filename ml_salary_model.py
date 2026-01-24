2e7537f48132e9f8a1bf0d867a7bf2967f546b6d
Dzeavo <Dzeavo@gmail.com>
2026-01-24 15:47:22 +0300

    Update ml_salary_model.py

--- ml_salary_model.py
+++ ml_salary_model.py
@@ -1,1 +1,23 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+# загружаем данные
+df = pd.read_csv("data.csv")
+
+# X — признак (возраст)
+X = df[["age"]]
+
+# y — то, что предсказываем (зарплата)
+y = df["salary"]
+
+# создаём модель
+model = LinearRegression()
+
+# обучаем модель
+model.fit(X, y)
+
+# делаем предсказание
+predicted_salary = model.predict([[30]])
+
+print("Predicted salary for age 30:", predicted_salary[0])
 
